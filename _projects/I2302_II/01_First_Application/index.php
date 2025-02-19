688919dfe0b747064cc0040db12f119bc3e7ae9f
/rdndev <rdninc@mail.ru>
2025-02-19 15:54:04 +0200

    feat: add posts

--- _projects/I2302_II/01_First_Application/index.php
+++ _projects/I2302_II/01_First_Application/index.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog App</title>
-    <link rel="stylesheet" href="./assets/styles/output.css">
-</head>
+define('APP_NAME', 'Blog App');
 
-<body>
-    <header class="border border-gray-200 py-3 px-8 lg:px-16">
-        <div class="mx-auto flex justify-between font-medium text-base">
-            <h1>Blog App</h1>
-            <a href="#">Login</a>
-        </div>
-    </header>
-</body>
+require_once './components/header.php';
 
-</html>
+?>
+
+<main>
+    Hello, World
+</main>
+
+<?php require_once './components/footer.php';
---
+++ _projects/I2302_II/data/posts.php
@@ -0,0 +1,19 @@
+<?php
+
+$posts = [
+    [
+        'title' => 'New Years Resolutions',
+        'content' => 'For the new year, I plan to eat healthier and exercise more.',
+        'date' => '2021-01-01',
+    ],
+    [
+        'title' => 'Valentines Day',
+        'content' => 'I spent Valentines Day with my partner and we had a great time.',
+        'date' => '2021-02-14',
+    ],
+    [
+        'title' => 'Saint Patricks Day',
+        'content' => 'I wore green and celebrated Saint Patricks Day with my friends.',
+        'date' => '2021-03-17',
+    ]
+];
