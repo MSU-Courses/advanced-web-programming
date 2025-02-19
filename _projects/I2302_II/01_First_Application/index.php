1d55f925a62a39fa4a1c5a41002ce7d53e73f38e
/rdndev <rdninc@mail.ru>
2025-02-19 16:02:46 +0200

    feat: output posts

--- _projects/I2302_II/01_First_Application/index.php
+++ _projects/I2302_II/01_First_Application/index.php
@@ -2,12 +2,25 @@
 
 define('APP_NAME', 'Blog App');
 
+require_once './data/posts.php';
+
 require_once './components/header.php';
 
 ?>
 
 <main>
-    Hello, World
+    <section id="posts" class="container mx-auto py-6 px-8">
+        <h1 class="font-bold text-2xl">#_posts</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <?php foreach ($posts as $post) : ?>
+                <div class="border border-gray-300 rounded-md p-4 mt-4 flex flex-col gap-3 col-span-1">
+                    <h2 class="font-bold text-xl"><?php echo $post['title']; ?></h2>
+                    <p class="text-gray-600"><?php echo $post['content']; ?></p>
+                    <i class="text-gray-500"><?php echo $post['date']; ?></i>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </section>
 </main>
 
 <?php require_once './components/footer.php';
