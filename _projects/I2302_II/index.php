ad38a468c79f889adec12f6f41d09a7f4189c741
/rdndev <rdninc@mail.ru>
2025-02-19 15:13:03 +0200

    feat: add project for group i2302_II

---
+++ _projects/I2302_II/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog App</title>
+    <link rel="stylesheet" href="./assets/styles/output.css">
+</head>
+
+<body>
+    <header class="border border-gray-200 py-3 px-8 lg:px-16">
+        <div class="mx-auto flex justify-between font-medium text-base">
+            <h1>Blog App</h1>
+            <a href="#">Login</a>
+        </div>
+    </header>
+</body>
+
+</html>
