95ff900801257dcf6c65ba1120f7c7338be35979
/rdndev <rdninc@mail.ru>
2025-02-24 12:56:32 +0200

    feat: add json

--- _projects/IA2303/01_First_Application/data/posts.php
+++ _projects/IA2303/01_First_Application/data/posts.php
@@ -1,19 +1,6 @@
 <?php
 
-$posts = [
-    [
-        'title' => 'New Years Resolutions',
-        'content' => 'New years resolutions are a great way to start the year off right. They can help you set goals and achieve them. Here are some tips to help you stick to your new years resolutions.',
-        'date' => '2021-01-01',
-    ],
-    [
-        'title' => 'Valentines Day Ideas',
-        'content' => 'Valentines day is a great time to show your loved ones how much you care. Here are some ideas to help you celebrate valentines day with your significant other.',
-        'date' => '2021-02-14',
-    ],
-    [
-        'title' => 'Saint Patricks Day Recipes',
-        'content' => 'Saint patricks day is a great time to celebrate with friends and family. Here are some recipes to help you celebrate saint patricks day with your loved ones.',
-        'date' => '2021-03-17',
-    ]
-];
+$postPath = __DIR__ . "/posts.json";
+
+$posts = file_exists($postPath) ?
+    json_decode(file_get_contents($postPath), true) : [];
