76ca407d646f96903b4d598cd431c0abb264d0ab
/rdndev <rdninc@mail.ru>
2025-02-25 11:14:44 +0200

    feat: add form validation

--- _projects/IA2303/01_First_Application/article-create.php
+++ _projects/IA2303/01_First_Application/article-create.php
@@ -4,9 +4,9 @@ define('PROJECT', 'Blog');
 
 require_once './data/posts.php';
 
-require_once './functions.php';
+require_once './functions/template.functions.php';
 
-require_once './handlers/post-handler.php';
+require_once './handlers/post.handler.php';
 
 require_once './components/header.php';
 
@@ -17,12 +17,13 @@ require_once './components/header.php';
     <form class="container mx-auto mt-8 space-y-4" method="post" action="/article-create.php">
         <div class="flex flex-col">
             <label for="title" class="mb-2 text-lg font-medium text-gray-700">Title</label>
-            <input type="text" id="title" name="title" required class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+            <input type="text" id="title" name="title" required class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value="<?php echo $_POST['title'] ?? ''; ?>" />
             <?php printFormError($errors, 'title'); ?>
         </div>
         <div class="flex flex-col">
             <label for="content" class="mb-2 text-lg font-medium text-gray-700">Content</label>
-            <textarea id="content" name="content" required class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+            <textarea id="content" name="content" required class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"><?php echo $_POST['content'] ?? ''; ?></textarea>
             <?php printFormError($errors, 'content'); ?>
         </div>
         <div class="flex flex-col">
@@ -31,7 +32,7 @@ require_once './components/header.php';
                 <option value="Technology">Technology</option>
                 <option value="Science">Science</option>
                 <option value="Art">Art</option>
-                <option value="Music">Music</json>
+                <option value="Music">Music</option>
             </select>
             <?php printFormError($errors, 'categories'); ?>
         </div>
--- _projects/IA2303/01_First_Application/functions/template.functions.php
+++ _projects/IA2303/01_First_Application/functions/template.functions.php
@@ -4,6 +4,15 @@ if (!defined('PROJECT')) {
     die();
 }
 
+
+/**
+ * Print form error
+ * 
+ * @param array $errors
+ * @param string $field
+ * 
+ * @return void
+ */
 function printFormError(array $errors, string $field)
 {
     foreach ($errors[$field] ?? [] as $error) {
